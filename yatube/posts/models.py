77e2ae1890c87876759b40e6697946b56e4185de
Полад Ходжаев <polaroid.kh@yandex.ru>
2023-05-05 01:34:24 +0400

    hw sprint 5

--- yatube/posts/models.py
+++ yatube/posts/models.py
@@ -1,50 +1,36 @@
-from django.db import models
 from django.contrib.auth import get_user_model
+from django.db import models
 
 User = get_user_model()
 
 
 class Group(models.Model):
-    title = models.CharField(
-        max_length=200,
-        verbose_name='Название группы',
-    )
-    slug = models.SlugField(
-        max_length=255,
-        unique=True,
-        null=False,
-        verbose_name='Слаг группы',
-    )
-    description = models.TextField(
-        verbose_name='Описание группы',
-    )
+    title = models.CharField(max_length=200)
+    slug = models.SlugField(unique=True)
+    description = models.TextField()
 
     def __str__(self):
         return self.title
 
 
 class Post(models.Model):
-    text = models.TextField(
-        verbose_name='Текст',
-    )
-    pub_date = models.DateTimeField(
-        auto_now_add=True,
-        verbose_name='Дата публикации',
-    )
+    text = models.TextField("Текст поста", help_text="Введите текст поста")
+    pub_date = models.DateTimeField("Дата публикации", auto_now_add=True)
     author = models.ForeignKey(
-        User,
-        verbose_name='Автор',
-        on_delete=models.CASCADE,
-        related_name='posts'
+        User, on_delete=models.CASCADE, related_name="posts", verbose_name="Автор"
     )
     group = models.ForeignKey(
         Group,
-        verbose_name='Группа',
         blank=True,
         null=True,
         on_delete=models.SET_NULL,
-        related_name='posts'
+        related_name="posts",
+        verbose_name="Группа",
+        help_text="Группа, к которой будет относиться пост",
     )
+
+    class Meta:
+        ordering = ["-pub_date"]
 
     def __str__(self):
         return self.text
